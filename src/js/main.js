b7f82dd38e21e708a39f1f012c1d2c5f2a67d445
russell goldenberg <russellgoldenberg@gmail.com>
2015-04-11 19:00:09 -0400

    fix #18 make globe iframe an external lib

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,24 @@
 'use strict';
 (function() {
-	/*** define your global variables here ***/
+	// global variables
 
 
-
+	// graphic functions
 	var init = function() {
-		setupPym(); // DO NOT TOUCH
-		
-		/*** call functions you want to run on load here ***/
-
+		if (window.console && console.log) console.log('-- init globe graphic --');
 
 
 	};
 
-	// This is called once when the page loads, and again whenever the parent window resizes
-	var parentResize = function() {
-		/*** put code here you want to run on window resize ***/
 
+
+
+	// This fires when the parent window resizes
+	window.onPymParentResize = function(width) {
 		
 
 	};
-
-	/*** (begin) your functions here ***/
 	
-
-
-
-
-
-
-
-
-
-	/************** (end) **************/
-
-
-	/************************************/
-	/* DANGER ZONE: DON'T GO BELOW HERE */
-	/************************************/
-	var setupPym = function() {
-		if (window.console && console.log) { console.log('-- init globe graphic --'); }
-		var container = document.getElementById('globe-graphic-container');
-		var pymChild = null;
-		var height = {previous: 0, current: 0};
-
-		var pollHeight = function() {
-			height.current = container.offsetHeight;
-			if(height.current !== height.previous) {
-				height.previous = height.current;
-				pymChild.sendHeight();
-			}
-			requestAnimationFrame(pollHeight);
-		};
-
-		pymChild = pym.Child({ renderCallback: parentResize });
-		/*** to get parent window height, see http://github.com/russellgoldenberg/globe-iframe-graphic#get-parent-height ***/
-
-		pollHeight();
-	};
+	// to get parent window height, see http://github.com/russellgoldenberg/globe-iframe-graphic#get-parent-height
 	init();
 })();
