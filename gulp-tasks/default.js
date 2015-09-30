7468cf69638c2a06486d26500d1b872bbfcd4db7
russell goldenberg <russellgoldenberg@gmail.com>
2015-09-30 10:14:05 -0400

    tweaks

--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 
 gulp.task('default', ['css-dev', 'js-dev', 'browser-sync'], function () {
-    gulp.watch('src/dev/*.styl', ['css-dev']);
-    gulp.watch('src/dev/*.js', ['js-dev']);
+    gulp.watch('src/dev/**/*.styl', ['css-dev']);
+    gulp.watch('src/dev/**/*.js', ['js-dev']);
     gulp.watch('src/dev/assets/**/*', ['assets-dev']);
     gulp.watch('src/dev/*.html', ['html-dev']);
 });
