1526be21b20178891dc91d95bf305b2c47e11799
russell goldenberg <russellgoldenberg@gmail.com>
2015-09-30 12:11:23 -0400

    css task

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
-var cmq = require('gulp-combine-media-queries');
 var browserSync = require('browser-sync');
 
 var src = 'src/dev/dev.styl';
@@ -11,8 +10,7 @@ gulp.task('css-dev', function () {
 	gulp.src(src)
         .pipe(stylus())
 		.pipe(autoprefixer())
-		.pipe(cmq({ log: true }))
 		.pipe(rename('main.css'))
 		.pipe(gulp.dest('src'))
-		.pipe(browserSync.reload({ stream: true }));
+		.pipe(browserSync.reload({stream:true}));
 });
