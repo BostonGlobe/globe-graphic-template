7ca85cef7496df92f1842a41bbc983217cf651af
Gabriel Florit <gabrielflorit@gmail.com>
2015-10-15 16:16:20 -0400

    Trim whitespace

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@
 
 	// called once on page load
 	var init = function() {
-		
+
 	};
 
 	// called automatically on page resize
 	window.onPymParentResize = function(width) {
-	
+
 	};
 
 	// graphic code
