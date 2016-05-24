c690a30613911776d46155ac29570873abb2b247
russellgoldenberg <russellgoldenberg@gmail.com>
2016-05-24 13:00:47 -0400

    remove dev tasks

--- gulp-tasks/default.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-
-gulp.task('default', ['html-dev', 'css-dev', 'js-dev', 'browser-sync'], function () {
-    gulp.watch('src/css/**/*.styl', ['css-dev'])
-    gulp.watch('src/js/**/*.js', ['js-dev'])
-    gulp.watch('src/assets/**/*', ['assets-dev'])
-    gulp.watch('src/index.html', ['html-dev'])
-})
